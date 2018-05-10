a59940ab793856991a3b2f109c3e6ae29d727792
liuhuanguang <923414405@qq.com>
2018-05-10 16:36:10 +0800

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Sample
+                    Sample Test
                 </div>
 
             </div>
